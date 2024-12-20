6e6a54e92369658fb18d136b31024273ad9c4e6f
Ahyeon, Jung <75254185+a-honey@users.noreply.github.com>
2024-12-20 14:28:03 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 9580 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 
 const [length, word] = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 let sum = 0;
+let now = 1;
 
 for (let i = 0; i < Number(length); i++){
     const code = word[i].charCodeAt(0) - 'a'.charCodeAt(0) + 1
-    sum += Math.pow(31, i)*code;
+    sum = (sum + now *code)%1234567891;
+    now = now * 31%1234567891
 }
 
 console.log(sum);
